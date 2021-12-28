f91898276f021cc4cfbd9e8c22c3d39135ff2dbd
jun111haha <jun111haha@gmail.com>
2021-12-29 00:47:51 +0900

    21.12.29

---
+++ src/SumForEx.java
@@ -0,0 +1,17 @@
+public class SumForEx {
+    public static void main(String[] args) {
+
+        sumfor(7);
+    }
+
+    static int sumfor(int num){
+        int sum = 0;
+
+        for(int i=0; i<= num; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+        return sum;
+    }
+}
---
+++ src/그사이모든정수합.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class 그사이모든정수합 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("a와 b를 포함하여 그 사이의 모든 정수의 합을 구합니다.");
+        System.out.print("a의 값：");
+        int a = stdIn.nextInt();
+        System.out.print("b의 값：");
+        int b = stdIn.nextInt();
+
+        System.out.println("정수 a, b 사이의 모든 정수의 합은 " + sumof(a, b) + "입니다.");
+    }
+
+    static int sumof(int a, int b){
+        int min =0;
+        int max =0;
+        int sum =0;
+
+        if(a > b){
+            min = b;
+            max = a;
+        }else{
+            min = a;
+            max = b;
+        }
+
+        for(int i=min; i<=max; i++){
+            sum+=i;
+        }
+
+        System.out.println(sum);
+        return sum;
+    }
+
+}
